3b3c4d2225d41b5938bbb58b7500ae81f5017511
JasonW88 <103770424+JasonW88@users.noreply.github.com>
2023-11-22 02:21:18 -0500

    Revert "Created functions to retrieve accelerations"
    This reverts commit bba66965f00211cad97348f7aac49fde80d5a1f8.

--- sensors.h
+++
@@ -1,7 +0,0 @@
-#include <Wire.h>
-#include <SPI.h>
-#include <Adafruit_LIS3DH.h>
-#include <Adafruit_Sensor.h>
-void get_accelerations(Adafruit_LIS3DH &accelerometer, float acc_array[]);
-void get_accelerations_raw(Adafruit_LIS3DH &accelerometer, short acc_array[]);
-void get_converted_raw_accelerations(Adafruit_LIS3DH &accelerometer, short raw_acc[], float acc_array[]);
