c90b38d869c118976bc653c8341dbe216429fa98
JasonW88 <103770424+JasonW88@users.noreply.github.com>
2023-11-22 03:35:00 -0500

    Added flex sensor, buzzer, and led code

--- back_sensor_arduino.ino
+++ back_sensor_arduino.ino
@@ -5,7 +5,7 @@
 #include <SPI.h>
 #include <Adafruit_LIS3DH.h>
 #include <Adafruit_Sensor.h>
-
+#include <LiquidCrystal_I2C.h>
 #define LIS3DH_CLK 13
 #define LIS3DH_MISO 12
 #define LIS3DH_MOSI 11
@@ -19,21 +19,23 @@ float std_dev{0};
 float curr_average{0};
 float num_vals{0};
 float prev_average{0};
-
-
+int flex_sensor = A0;
+int value = 0;
+int flex{950};
+int rating{1};
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
 
   if (! lis3dh.begin(0x18)) {
     while (1) {
-      yield();      //code snippet provided from Adafruit, can be found at https://github.com/adafruit/Adafruit_LIS3DH
+      yield();      //Function provided from Adafruit, can be found at https://github.com/adafruit/Adafruit_LIS3DH
     }
   }
   prevTimeMillis = millis();
   currTimeMillis = millis();
+  pinMode(D5, OUTPUT);
 }
-
 
 void loop() {
   // put your main code here, to run repeatedly:
@@ -49,10 +51,27 @@ void loop() {
     std_dev = calculate_std_dev(variance);
   }
 
-  if (to_degrees(curr_angle) >= ANGLE_THRESHOLD) {
-    //beep beep beep
+if (to_degrees(curr_angle) >= ANGLE_THRESHOLD || value >= flex) {
+  digitalWrite(D5, HIGH);
+} else {
+  digitalWrite(D5, LOW);
+}
+  Serial.print("Angle: ");
+  Serial.println(to_degrees(curr_angle));
+
+  value = analogRead(flex_sensor);
+  Serial.println(value);
+  if (value >= flex) {
+    //digitalWrite(D5, HIGH);
+    rating = 3;
+  } else if (value >= flex - 75) {
+    //digitalWrite(D5, LOW);
+    rating = 2;
   } else {
-    //no beep
+    //digitalWrite(D5, LOW);
+    rating = 1;
   }
+  Serial.print(rating);
+  delay(200);
 
 }
