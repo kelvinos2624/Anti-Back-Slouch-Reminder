736386b759e00b2837ea8e2cd652d57397fd4fef
JasonW88 <103770424+JasonW88@users.noreply.github.com>
2023-11-22 01:07:08 -0500

    Implemented math to get angle from acceleration
    Also made a function to convert radians to degrees and declared constants that hold the angle threshold, acc due to gravity, and pi (with 6 digits)

---
+++ angle_math.h
@@ -0,0 +1,16 @@
+float get_angle(float z_comp_acc);
+float to_degrees(float radians);
+
+/*
+Constants
+--------------
+*/
+
+//Acceleration due to gravity in m/s^2
+float const GRAVITY_ACC{9.81};
+
+//An approximation of the mathematical constant pi with 6 digits
+float const PI_6_DIG{3.14159}; 
+
+//The angle threshold for slouching measured in degrees
+float const ANGLE_THRESHOLD{15};
